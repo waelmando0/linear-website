89288315a5b42394e5b0668801b0595c7c310a25
waelmando0 <59392618+waelmando0@users.noreply.github.com>
2023-03-05 09:02:14 +0200

    update nav

--- app/header.tsx
+++ app/header.tsx
@@ -27,60 +27,42 @@ const header = () => {
 			window.removeEventListener('resize', closeMenuNavigation);
 		};
 	}, [setMenuIsOpen]);
-
 	return (
 		<header className='fixed top-0 left-0 w-full backdrop-blur-[12px]'>
 			<Container className='flex h-navigation-height border-b border-custom-border'>
+				{/* Logo */}
 				<Link href='/' className='flex items-center'>
 					<Logo />
 				</Link>
 
-				<div
-					className={classNames(
-						'transition-[visibility] md:visible',
-						menuIsOpen ? 'visible' : 'invisible delay-500'
-					)}
-				>
-					<nav
-						className={classNames(
-							'fixed md:relative md:top-0 md:block top-navigation-height left-0 w-full h-[calc(100vh_-_var(--navigation-height))] md:h-auto md:w-auto md:bg-transparent md:overflow-hidden overflow-auto bg-background md:opacity-100 transition-opacity duration-500 md:translate-x-0',
-							menuIsOpen
-								? 'translate-x-0 opacity-100'
-								: 'translate-x-[100vw] opacity-0'
-						)}
-					>
-						<ul
-							className={classNames(
-								'flex flex-col md:flex-row md:items-center h-full [&_li]:ml-4',
-								'ease-in [&_a]:duration-300 [&_a]:translate-y-8 md:[&_a]:translate-y-0  [&_a]:flex [&_a]:items-center [&_a]:h-navigation-height [&_a]:w-full [&_a]:transition-[color, transform] [&_li]:border-none [&_a]:text-md md:[&_a]:text-sm [&_a]:font-medium [&_a:hover]:text-gray',
-								menuIsOpen && '[&_a]:translate-y-0'
-							)}
-						>
-							<li>
-								<Link href='#'>Features</Link>
-							</li>
-							<li>
-								<Link href='#'>Method</Link>
-							</li>
-							<li className='md:hidden lg:block'>
-								<Link href='#'>Customers</Link>
-							</li>
-							<li className='md:hidden lg:block'>
-								<Link href='#'>Changelog</Link>
-							</li>
-							<li className='md:hidden lg:block'>
-								<Link href='#'>Integrations</Link>
-							</li>
-							<li>
-								<Link href='#'>Pricing</Link>
-							</li>
-							<li>
-								<Link href='#'>Company</Link>
-							</li>
-						</ul>
-					</nav>
-				</div>
+				{/* Desktop Nav */}
+				<nav className='hidden md:flex'>
+					<ul className='flex items-center justify-center'>
+						<li>
+							<Link href='#'>Features</Link>
+						</li>
+						<li>
+							<Link href='#'>Method</Link>
+						</li>
+						<li className='md:hidden lg:block'>
+							<Link href='#'>Customers</Link>
+						</li>
+						<li className='md:hidden lg:block'>
+							<Link href='#'>Changelog</Link>
+						</li>
+						<li className='md:hidden lg:block'>
+							<Link href='#'>Integrations</Link>
+						</li>
+						<li>
+							<Link href='#'>Pricing</Link>
+						</li>
+						<li>
+							<Link href='#'>Company</Link>
+						</li>
+					</ul>
+				</nav>
 
+				{/* Links */}
 				<div className='ml-auto flex items-center'>
 					<Link href='#' className='text-sm mr-4'>
 						Log in
@@ -88,11 +70,12 @@ const header = () => {
 					<Button href='#'>Sign up</Button>
 				</div>
 
-				<div
-					className='flex  md:hidden ml-4'
+				{/* Mobile Nav */}
+				<nav
+					className='flex md:hidden ml-4'
 					onClick={() => setMenuIsOpen((open) => !open)}
 				>
-					<button className={menuIsOpen ? 'l active' : 'l'}>
+					<button className={menuIsOpen ? 'l active z-30' : 'l'}>
 						<span className='sr-only'>Toggle menu</span>
 						<svg
 							className='w-3 h-3 text-white fill-current '
@@ -104,7 +87,37 @@ const header = () => {
 							<rect y='18' width='24' height='2'></rect>
 						</svg>
 					</button>
-				</div>
+					{menuIsOpen && (
+						<div className='absolute left-0 pt-16 top-0 w-full h-screen pb-16 z-[-2] bg-background'>
+							<ul className='py-2 px-5 sm:px-6'>
+								<li>
+									<Link
+										href='/'
+										className='flex items-center py-4  text-gray-600 font-semibold text-sm tracking-wide  border-b border-gray-300'
+									>
+										About Me
+									</Link>
+								</li>
+								<li>
+									<Link
+										href='/'
+										className='flex items-center py-4  text-gray-600 font-semibold text-sm tracking-wide  border-b border-gray-300'
+									>
+										About Me
+									</Link>
+								</li>
+								<li>
+									<Link
+										href='/'
+										className='flex items-center py-4  text-gray-600 font-semibold text-sm tracking-wide  border-b border-gray-300'
+									>
+										About Me
+									</Link>
+								</li>
+							</ul>
+						</div>
+					)}
+				</nav>
 			</Container>
 		</header>
 	);
